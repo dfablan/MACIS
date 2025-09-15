2b4489b08087f39c2dcf8ea9029ee8579f4cdc9f
Clang Robot <robot@example.com>
2025-09-15 12:57:41 +0000

    Committing clang-format changes

--- include/macis/comp_observables.hpp
+++ include/macis/comp_observables.hpp
@@ -22,10 +22,10 @@ void Transform_2RDMs(const int norbs, const std::vector<double>& ordm_u,
   for(int o1 = 0; o1 < norbs; o1++)
     for(int v1 = 0; v1 < norbs; v1++)
       for(int e = 0; e < norbs; e++) {
-        trdm_uu[v1 + e * norbs + e * norbs * norbs +  o1 * norbs * norbs * norbs  ] +=
-            ordm_u[v1 + norbs * o1];
-        trdm_dd[v1 + e * norbs + e * norbs * norbs +  o1 * norbs * norbs * norbs  ] +=
-            ordm_d[v1 + norbs * o1];
+        trdm_uu[v1 + e * norbs + e * norbs * norbs +
+                o1 * norbs * norbs * norbs] += ordm_u[v1 + norbs * o1];
+        trdm_dd[v1 + e * norbs + e * norbs * norbs +
+                o1 * norbs * norbs * norbs] += ordm_d[v1 + norbs * o1];
       }
 }
 
@@ -291,10 +291,11 @@ class CompObservables {
           for(int c = 0; c < n_imp_; c++)
             for(int d = 0; d < n_imp_; d++) {
               orb_db_occs +=
-                orb_rot_[i+a*n_active_]*orb_rot_[i+c*n_active_]*
-                trdm_ud_[a + b * n_active_ + c * n_active2_ + d * n_active3_]*
-                orb_rot_[i+b*n_active_]*orb_rot_[i+d*n_active_];
-              }
+                  orb_rot_[i + a * n_active_] * orb_rot_[i + c * n_active_] *
+                  trdm_ud_[a + b * n_active_ + c * n_active2_ +
+                           d * n_active3_] *
+                  orb_rot_[i + b * n_active_] * orb_rot_[i + d * n_active_];
+            }
     return orb_db_occs / n_imp_;
   }
 
@@ -306,19 +307,24 @@ class CompObservables {
           int i = site_i + n_sites_ * band_i;
           for(size_t band_j = 0; band_j < n_bands_; band_j++) {
             int j = site_j + n_sites_ * band_j;
-            for(size_t a = 0; a < n_imp_; a++) 
-              for(size_t b = 0; b < n_imp_; b++) 
-                for(size_t c = 0; c < n_imp_; c ++) 
-                  for(size_t d = 0; d < n_imp_; d++){
+            for(size_t a = 0; a < n_imp_; a++)
+              for(size_t b = 0; b < n_imp_; b++)
+                for(size_t c = 0; c < n_imp_; c++)
+                  for(size_t d = 0; d < n_imp_; d++) {
                     sz_sz[site_i + site_j * n_sites_] +=
-                       0.25 *
-                      orb_rot_[i+a*n_active_]*orb_rot_[j+c*n_active_]*
-                      (  trdm_uu_[a + b * n_active_ + c * n_active2_ + d * n_active3_]
-                       - trdm_ud_[a + b * n_active_ + c * n_active2_ + d * n_active3_]
-                       - trdm_du_[a + b * n_active_ + c * n_active2_ + d * n_active3_]
-                       + trdm_dd_[a + b * n_active_ + c * n_active2_ + d * n_active3_])
-                      *orb_rot_[i+b*n_active_]*orb_rot_[j+d*n_active_];
-                    }
+                        0.25 * orb_rot_[i + a * n_active_] *
+                        orb_rot_[j + c * n_active_] *
+                        (trdm_uu_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_] -
+                         trdm_ud_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_] -
+                         trdm_du_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_] +
+                         trdm_dd_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_]) *
+                        orb_rot_[i + b * n_active_] *
+                        orb_rot_[j + d * n_active_];
+                  }
           }
         }
       }
@@ -337,16 +343,21 @@ class CompObservables {
             double sign = (band_i == band_j) ? 1.0 : -1.0;
             for(size_t a = 0; a < n_imp_; a++)
               for(size_t b = 0; b < n_imp_; b++)
-                for(size_t c = 0; c < n_imp_; c++) 
-                  for(size_t d = 0; d < n_imp_; d++){
+                for(size_t c = 0; c < n_imp_; c++)
+                  for(size_t d = 0; d < n_imp_; d++) {
                     tz_tz[site_i + site_j * n_sites_] +=
-                        0.25 * sign *
-                      orb_rot_[i+a*n_active_]*orb_rot_[j+c*n_active_]*
-                        (  trdm_uu_[a + b * n_active_ + c * n_active2_ + d * n_active3_]
-                         + trdm_ud_[a + b * n_active_ + c * n_active2_ + d * n_active3_]
-                         + trdm_du_[a + b * n_active_ + c * n_active2_ + d * n_active3_]
-                         + trdm_dd_[a + b * n_active_ + c * n_active2_ + d * n_active3_])*
-                        orb_rot_[i+b*n_active_]*orb_rot_[j+d*n_active_];
+                        0.25 * sign * orb_rot_[i + a * n_active_] *
+                        orb_rot_[j + c * n_active_] *
+                        (trdm_uu_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_] +
+                         trdm_ud_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_] +
+                         trdm_du_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_] +
+                         trdm_dd_[a + b * n_active_ + c * n_active2_ +
+                                  d * n_active3_]) *
+                        orb_rot_[i + b * n_active_] *
+                        orb_rot_[j + d * n_active_];
                   }
           }
         }
