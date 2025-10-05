a1dc8b6198dc6e581d13734c9acedf1d0d0ecabe
Diego Florez Ablan <dfloreza@sissa.it>
2025-10-05 11:25:55 +0200

    Small doping counting issue fixed

--- src/macis/impurity_solver.cpp
+++ src/macis/impurity_solver.cpp
@@ -406,8 +406,8 @@ double SolveImpurityASCI_rot (impurity_params<N>& p){
       std::cout << "occs[" << i << "] = " << occs[i] << std::endl;
     }
 
-    double curr_nel_per_spin = std::accumulate(occs.begin(), occs.begin()+ n_imp, 0.0); //DEBUG
-    std::cout << "* Number of electrons on impurity (per spin) = " << curr_nel_per_spin << std::endl;
+    double curr_nel_per_spin = std::accumulate(occs.begin(), occs.begin()+ n_imp, 0.0)/n_imp; //DEBUG
+    std::cout << "* Number of electrons on impurity (per orbital per spin) = " << curr_nel_per_spin << std::endl;
 
     bool print_ordm = false;
     if (print_ordm)
