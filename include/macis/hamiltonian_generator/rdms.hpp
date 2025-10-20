7df651db91878120e7a693e27fe3784d7da1d451
Diego Florez Ablan <dfloreza@sissa.it>
2025-10-20 14:31:34 +0200

    upup

--- include/macis/hamiltonian_generator/rdms.hpp
+++ include/macis/hamiltonian_generator/rdms.hpp
@@ -128,16 +128,7 @@ void HamiltonianGenerator<N>::rotate_hamiltonian_ordm_imp_bath(
 
   const int nbaths = norb_ - nimps;
   // SVD on ordm to get natural orbitals
-
-#!/bin/bash
-
-#SBATCH --job-name=serial_job             # Descriptive name for the job
-#SBATCH --time=00:30:00                   # Maximum wall time (hh:mm:ss)
-#SBATCH --nodes=1                         # Request one node
-#SBATCH --ntasks=1                        # One task (process) total
-#SBATCH --cpus-per-task=1                 # One CPU core per task
-#SBATCH --partition=<partition_na
-
+  
   std::vector<double> natural_orbitals(norb2_, 0.);
 
   std::vector<double> nat_orbs_imp(nimps * nimps, 0.);
