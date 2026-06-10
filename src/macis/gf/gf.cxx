3c60cea783d4e54f60ffc0838ca5a403b9f0d5c3
Clang Robot <robot@example.com>
2026-06-10 09:39:38 +0000

    Committing clang-format changes

--- src/macis/gf/gf.cxx
+++ src/macis/gf/gf.cxx
@@ -131,7 +131,7 @@ void write_GF(const std::vector<std::vector<std::complex<double>>> &GF,
     std::string fname = "GF.dat";
     // std::string fname = "GF_";
     // fname += std::to_string(GF_orbs[0] + 1) + "_" +
-            //  std::to_string(GF_orbs[0] + 1) + ".dat";
+    //  std::to_string(GF_orbs[0] + 1) + ".dat";
     std::ofstream ofile(fname);
     ofile.precision(dbl::max_digits10);
     for(int iii = 0; iii < nfreqs; iii++)
